3f563c72b48fe562fc4303fc72877d20eed8fbb8
KrazyGenus <bugfix.exe@gmail.com>
2025-06-25 17:23:22 +0100

    Added a demo scripts that talking to the nsfw model and a gemeni vision model as a fallback when nsfw is unsure about the image sent for detection e.g the nsfw is around 40%, this would trigger a send to the google model

---
+++ backend/src/inference/nsfw_detector.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const path = require('path');
+require('dotenv').config();
+
+
+
+async function modelNSFWHuggingFace(imagePath) {
+  const buffer = fs.readFileSync(imagePath);
+
+  const response = await fetch(
+    'https://api-inference.huggingface.co/models/Falconsai/nsfw_image_detection',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+        'Content-Type': 'image/jpeg',
+      },
+      body: buffer,
+    }
+  );
+
+  const result = await response.json();
+  result.forEach((item) => {
+    if (item.label === 'nsfw') {
+      if (item.score <= 0.0001419968466507271) {
+        console.log('not safe!');
+      }
+    }
+  })
+  return result;
+}
+
+modelNSFWHuggingFace('/home/krazygenus/Desktop/a.jpg')
+  .then((res) => console.log(JSON.stringify(res)))
+  .catch(console.error);
+
+module.exports = { modelNSFWHuggingFace };
